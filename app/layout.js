4b79bd479ec24eea87e443e3802a216f0c915ecb
adriendelagneau <delagneauadrien@yahoo.fr>
2024-01-16 12:06:14 +0100

    first commit

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,9 @@
 import { Inter } from 'next/font/google'
 import AuthProvider from '@/components/providers/AuthProvider'
 import SmoothScrolling from '@/components/providers/LennisProvider'
-import { ToastContainer } from 'react-toastify'
+//import { ToastContainer } from 'react-toastify'
 import './globals.css'
-import 'react-toastify/dist/ReactToastify.css';
+//import 'react-toastify/dist/ReactToastify.css';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,18 +17,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <AuthProvider>
-        <ToastContainer
-            position="bottom-right"
-            autoClose={2500}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme="dark"
-          />
+       
           <div className='text-skin-base'>
             <SmoothScrolling>
               {children}
